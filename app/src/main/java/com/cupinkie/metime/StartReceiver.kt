087378f720c512cdda4d1db8f3e5e3909b2c1b73
zanjie1999 <zanjie1999@yeah.net>
2020-08-14 16:28:49 +0800

    开机自启

---
+++ app/src/main/java/com/cupinkie/metime/StartReceiver.kt
@@ -0,0 +1,26 @@
+package com.cupinkie.metime
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import androidx.preference.PreferenceManager
+
+class StartReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val type = intent.getAction().toString();
+//        if (type.equals(Intent.ACTION_BOOT_COMPLETED)) {}
+        // 检查下设置是否需要自启
+        val sp = PreferenceManager.getDefaultSharedPreferences(context)
+        val startAtBooted = sp.getBoolean("startAtBooted", false)
+        Log.v("metime", "startAtBooted："+ startAtBooted)
+        if (startAtBooted) {
+            Toast.makeText(context, "咩时间启动啦~", Toast.LENGTH_LONG)
+            val intent = Intent(context, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(intent)
+        }
+    }
+}
